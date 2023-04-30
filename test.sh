a0ca20aee6b2e3b52e2fe2d3c1b0da70ad3d760d
mattseabrook <info@mattseabrook.net>
2023-04-30 18:07:31 +0000

    .

--- test.sh
+++ test.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-: '
-autogpt-deploy.sh
-
-v.0.1 - 2023-04-28 - Matt Seabrook
-
-See README.md or CTRL + F "Main Entrypoint" in this file
-'
 
 APP_PORT=4000
-DOCKER_IMAGE="autogpt-docker-image"
-DOCKER_CONTAINER="autogpt-docker-container"
+DOCKER_IMAGE="test"
+DOCKER_CONTAINER="test_container"
 
 #
 # Function to display cool ASCII text
@@ -32,55 +25,6 @@ info@lakeshoretechnical.com
 EOF
 }
 
-: '
-/*
-================
-create_new_script
-
-Create a new shell script based on this one
-================
-*/
-'
-function create_new_script {
-    echo
-
-    read -p "Enter the name of the project: " project_name
-    read -p "Enter the port number: " port_number
-
-    # Replace spaces with underscores
-    safe_project_name=$(echo "$project_name" | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g')
-    new_script_name="${safe_project_name}.sh"
-
-    # Replace unique aspects in the shell script
-    cp "$0" "$new_script_name"
-    sed -i -e "s/autogpt-docker-image/${safe_project_name}/g" \
-        -e "s/autogpt-docker-container/${safe_project_name}_container/g" \
-        -e "s/APP_PORT=4000/APP_PORT=${port_number}/g" \
-        -e '/-g, --generate/d' \
-        -e '/create_new_script/,/^}/d' \
-        -e 's/.*-g, --generate.*//g' \
-        "$new_script_name"
-
-    echo -e 'function reinvoke_build_and_create() {\n  script_name=$(basename "$0")\n  ./"$script_name" -b && ./"$script_name" -c\n}\n' >>"$new_script_name"
-    sed -i 's/create_new_script/reinvoke_build_and_create/g' "$new_script_name"
-
-    sed -i -e '/: '\''$/,/^'\''$/d' -e '/^\/\*/,/^================$/d' "$new_script_name" # Remove multiline comments and stray characters
-
-    chmod +x "$new_script_name"
-    echo -e "\nNew shell script created: ${new_script_name}\n"
-}
-
-: '
-/*
-================
-cli_interface
-
-Process CLI arguments
-
-@param      $@      string(s)       All of the ARGVs provided via stdin, $2, $3, etc.
-================
-*/
-'
 function cli_interface {
     case $1 in
     # Attach to the Docker Container
@@ -115,27 +59,7 @@ function cli_interface {
         ;;
     # No arguments
     "")
-        create_new_script
-        ;;
-    *)
-        echo "ERROR: Invalid command line arguments/parameters: $@"
-        echo
-        echo "Usage: ./autogpt-deploy.sh {switch} {parameter}"
-        echo
-        cli_help
-        ;;
-    esac
-}
 
-: '
-/*
-================
-cli_help
-
-Print help related text on the screen like a standard CLI application
-================
-*/
-'
 function cli_help {
     echo
     echo -e "Usage: ./autogpt-deploy.sh\t{switch} {parameters...}"
@@ -143,7 +67,6 @@ function cli_help {
     echo -e "\t-a, --attach\tAttach to this project's Docker Container."
     echo -e "\t-b, --build\tBuild Docker Image as specified in the Dockerfile."
     echo -e "\t-c, --create\tCreate & Run the Docker Container in terminal."
-    echo -e "\t-g, --generate\tGenerate a new unique version of this shell script."
     echo -e "\t-x, --remove\tRemove the Docker Image and Container."
     echo -e "\t-h, --help\tDisplay this help text."
     echo
@@ -151,17 +74,11 @@ function cli_help {
     echo
 }
 
-: '
-//==========================================================================
-
-/*
-================
-Main Entrypoint
-
-Upon execution of the shell script
-================
-*/
-'
 
 display_ascii
 cli_interface "$@"
+function reinvoke_build_and_create() {
+  script_name=$(basename "$0")
+  ./"$script_name" -b && ./"$script_name" -c
+}
+
